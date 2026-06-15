bf0fb97b54893790c8563916c1007c368ad76ce3
DucAnh1302 <ducanh1402006@gmail.com>
2026-06-16 03:32:56 +0700

    Add AuditLog for Project

---
+++ QLNhaXac/QuanLyNhaXac/DoAn/Views/UserControls/AuditLog.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace DoAn.Views.UserControls
+{
+    public partial class AuditLog : UserControl
+    {
+        public AuditLog()
+        {
+            InitializeComponent();
+        }
+    }
+}
